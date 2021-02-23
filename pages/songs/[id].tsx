ec704f2294e9eb49a45d9cfaa911bd42e12f2d63
Marcel Wiemers <77615352+m-wiemers@users.noreply.github.com>
2021-02-23 17:25:32 +0100

    🎉 add media Player (#8)
    * add media Player
    
    * add favorite Button

--- pages/songs/[id].tsx
+++ pages/songs/[id].tsx
@@ -4,46 +4,45 @@ import { APISong, getSong } from "../../utils/api";
 import SongContent from "../../components/Song";
 import Songnavigation from "../../components/Songnavigation";
 import AudioPlayer from "../../components/AudioPlayer";
+import styles from "../../styles/id.module.css";
 
 export default function Song() {
   const router = useRouter();
   const { id } = router.query;
-
   const [song, setSong] = useState<APISong>(null);
 
   useEffect(() => {
     if (typeof id !== "string") {
       return;
     }
-    getSong(id).then((newSong) => {
-      setSong(newSong);
-    });
+    getSong(id).then((newSong) => setSong(newSong));
   }, [id]);
 
   if (!song) {
     return <div>Loading...</div>;
   }
 
-  const songItem = (
-    <SongContent
-      image={song.image}
-      title={song.title}
-      artist={song.artist}
-      path={song.path}
-    />
-  );
-
   function goBack() {
     window.history.back();
   }
 
-  const path = song.path;
-
   return (
-    <>
+    <div className={styles.globContainer}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <Songnavigation back={goBack} />
-      <div>{songItem}</div>
-      <AudioPlayer audio={path} />
-    </>
+      <main>
+        <SongContent
+          image={song.image}
+          title={song.title}
+          artist={song.artist}
+          path={song.path}
+        />
+      </main>
+      <footer>
+        <AudioPlayer audio={song.path} id={song.id} />
+      </footer>
+    </div>
   );
 }
